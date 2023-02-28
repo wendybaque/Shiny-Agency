40b6153a5bacbb30cae326c21bbcc22f5a7f156a
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-02-28 11:29:02 +0100

    Fin leçon 8 hooks et fetch

--- src/pages/Freelances.jsx
+++ src/pages/Freelances.jsx
@@ -1,8 +1,8 @@
-import Card from "../components/Card";
-import styled from "styled-components";
-import colors from "../utils/style/colors";
-import { useState, useEffect } from "react";
-import { Loader } from "../utils/style/Atoms";
+import Card from '../components/Card'
+import styled from 'styled-components'
+import colors from '../utils/style/colors'
+import { Loader } from '../utils/style/Atoms'
+import { useFetch, useTheme } from '../utils/hooks'
 
 const CardsContainer = styled.div`
   display: grid;
@@ -11,63 +11,50 @@ const CardsContainer = styled.div`
   grid-template-columns: repeat(2, 1fr);
   align-items: center;
   justify-items: center;
-`;
+`
 
-const PageTitle = styled.h2`
+const PageTitle = styled.h1`
   font-size: 30px;
-  color: black;
   text-align: center;
   padding-bottom: 30px;
-`;
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+`
 
-const PageSubtitle = styled.h3`
+const PageSubtitle = styled.h2`
   font-size: 20px;
   color: ${colors.secondary};
   font-weight: 300;
   text-align: center;
   padding-bottom: 30px;
-`;
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+`
 
 const LoaderWrapper = styled.div`
   display: flex;
   justify-content: center;
-`;
+`
 
-const Freelances = () => {
-  const [isDataLoading, setDataLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [freelancersList, setFreelancesList] = useState([]);
+const Freelances =() => {
+  const { theme } = useTheme()
+  const { data, isLoading, error } = useFetch(
+    `http://localhost:8000/freelances`
+  )
 
-  useEffect(() => {
-    async function fetchFreelances() {
-      setDataLoading(true);
-      try {
-        const response = await fetch(`http://localhost:8000/freelances`);
-        const { freelancersList } = await response.json();
-        setFreelancesList(freelancersList);
-      } catch (err) {
-        console.log("===== error =====", err);
-        setError(true);
-      } finally {
-        setDataLoading(false);
-      }
-    }
-    fetchFreelances();
-  }, []);
+  const freelancersList = data?.freelancersList
 
   if (error) {
-    return <span>Oups il y a eu un problème</span>;
+    return <span>Oups il y a eu un problème</span>
   }
 
   return (
     <div>
-      <PageTitle>Trouvez votre prestataire</PageTitle>
-      <PageSubtitle>
+      <PageTitle theme={theme}>Trouvez votre prestataire</PageTitle>
+      <PageSubtitle theme={theme}>
         Chez Shiny nous réunissons les meilleurs profils pour vous.
       </PageSubtitle>
-      {isDataLoading ? (
+      {isLoading ? (
         <LoaderWrapper>
-          <Loader />
+          <Loader theme={theme} />
         </LoaderWrapper>
       ) : (
         <CardsContainer>
@@ -82,7 +69,8 @@ const Freelances = () => {
         </CardsContainer>
       )}
     </div>
-  );
+  )
 }
 
-export default Freelances;
+export default Freelances
+
