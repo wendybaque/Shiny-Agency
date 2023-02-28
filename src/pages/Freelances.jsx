32865e8e0526c3aeeac9f833ab02dfcc85b1ca15
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-02-28 06:48:02 +0100

    Fin leçon 3 propTypes

--- src/pages/Freelances.jsx
+++ src/pages/Freelances.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import DefaultPicture from "../assets/profile.png";
+import Card from "../components/Card";
 
 const Freelances = () => {
+  const freelanceProfiles = [
+    {
+      name: "Jane Doe",
+      jobTitle: "Devops",
+      picture: DefaultPicture,
+    },
+    {
+      name: "John Doe",
+      jobTitle: "Developpeur frontend",
+      picture: DefaultPicture,
+    },
+    {
+      name: "Jeanne Biche",
+      jobTitle: "Développeuse Fullstack",
+      picture: DefaultPicture,
+    },
+  ];
   return (
-    <div>Freelances</div>
-  )
-}
+    <div>
+      <h2>Freelances</h2>
+      {freelanceProfiles.map((profile, index) => (
+        <Card
+          key={`${profile.name}-${index}`}
+          label={profile.jobTitle}
+          picture={profile.picture}
+          title={profile.name}
+        />
+      ))}
+    </div>
+  );
+};
 
-export default Freelances
+export default Freelances;
